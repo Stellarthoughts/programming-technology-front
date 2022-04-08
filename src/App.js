4d3faf101f5cf57bee8f8af0b23eaf7858466e3c
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-08 15:18:32 +0700

    add: Friendship!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const [state, setState] = React.useState({
+		data: null
+	});
+
+	React.useEffect(() => {
+		componentDidMount();
+	}, []);
+
+	function componentDidMount() {
+    callBackendAPI()
+      .then(res => setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+	const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +34,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+				<p>
+					{state.data}
+				</p>
         <a
           className="App-link"
           href="https://reactjs.org"
