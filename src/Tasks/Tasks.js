b0a33bdbbd2e3ccbd4ccd7ee14c68a360cd5e9b1
Melly5 <polina.alina.2015@mail.ru>
2022-04-14 21:41:11 +0700

    add: tasks structure
    :3

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -1,7 +1,51 @@
+
+import React from 'react';
+import './Tasks.css';
 function TasksPage() {
+
+	const list = [
+		{
+			id: 1,
+			name: "name1",
+			content: "ddd0",
+		},
+		{
+			id: 1,
+			name: "name2",
+			content: "ddd0",
+		}
+	]
+
 	return (
-		<div>Таски</div>
+		<div className='tasks'>
+			<h1>Polina's production </h1>
+				<input
+			  	placeholder='Add a todo task'
+          type="text"
+          className="todo-input" 
+        />
+        <button type="submit" className="button-add" >
+          Add
+        </button>
+
+				<div className='todolist'>
+					<ul>
+					{
+						list.map((x) => {
+							return(
+								<div>
+									<li>
+										<input type="checkbox" className="custom-checkbox" value={x.name}/>
+									</li>
+								</div>
+							);
+						})
+					}
+					</ul>
+				</div>
+	
+		</div>
 	);
 }
 
-export default TasksPage;
+export default TasksPage; 
