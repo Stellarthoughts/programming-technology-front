4e09eb54cb88c3db85b296519806f0888a7584be
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-14 13:16:29 +0700

    Request to back-end from front page
    * fix: Response printing
    
    *add: Post request 
    
    * fix: Package-lock updates
    
    Co-authored-by: Aleksander Bekrenev <praise2001@yandex.ru>

--- src/App.js
+++ src/App.js
@@ -4,23 +4,42 @@ import './App.css';
 
 function App() {
 	const [state, setState] = React.useState({
-		data: null
+		data: 'nothing so far'
 	});
 
 	// ComponentDidMount
-	React.useEffect(() => {
-		callBackendAPI()
-      .then(res => setState({ data: res.express }))
-      .catch(err => console.log(err));
-	}, []);
+	// React.useEffect(() => {
+	// 	callBackendAPI()
+  //     .then(res => setState({ data: res.express }))
+  //     .catch(err => console.log(err));
+	// }, []);
 
 	const callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+		const login = 'hey Jude'
+		const email = '12345@test'
+		const password = '12345678'
+
+		const dataPost = {
+			login: login,
+			email: email,
+			password: password
+		}
+
+		const configInit = {
+			method: "POST",
+			body: JSON.stringify(dataPost),
+			headers: { 'Content-Type': 'application/json' }
+		}
+		const request = "/users"
+
+    const response = await fetch(request, configInit);
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message)
     }
+
+		setState({data: body})
     return body;
   };
 
@@ -31,17 +50,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-				<p>
-					{state.data}
-				</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+				<button onClick={() => callBackendAPI()}>
+					<p>{"Отправить инфу"}</p>
+				</button>
+        <p>{state.data.message}</p>
       </header>
     </div>
   );
