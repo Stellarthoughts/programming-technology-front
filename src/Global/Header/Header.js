7adebbc2dd0117e6d2b7205d2624f2c85e55472e
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-16 15:01:38 +0700

    feat: Header

--- src/Global/Header/Header.js
+++ src/Global/Header/Header.js
@@ -1,7 +1,14 @@
-function Header() {
+import Logo from "./Logo/Logo";
+import Bar from "./Bar/Bar";
+import "./Header.css"
+
+function Header(props) {
 	return (
-		<div>Шапочка</div>
-	); /* */
+		<div id="Header">
+			<div id="Logo"><Logo pageName={props.signed}/></div>
+			<div id="Bar"><Bar {...props}/></div>
+		</div>
+	);
 }
 
 export default Header;
---
+++ src/Global/Header/Logo/Logo.js
@@ -0,0 +1,7 @@
+function Logo(props) {
+	return (
+		<div>Logo</div>
+	);
+}
+
+export default Logo;
