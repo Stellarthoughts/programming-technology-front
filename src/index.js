9a53afc9154bb4a19d4562c64fa775c984dfcd6e
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-21 19:46:04 +0700

    feat: dark theme

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { AppThemeDark } from './Theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+		<ThemeProvider theme={AppThemeDark}>
+			<CssBaseline enableColorScheme/>
+    	<App />
+		</ThemeProvider>
   </React.StrictMode>,
 );
 
