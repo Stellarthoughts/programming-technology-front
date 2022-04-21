59ecbd76fe1c486f8203fcddff2e0fbb94ab8754
Melly5 <polina.alina.2015@mail.ru>
2022-04-22 01:39:43 +0700

    fix: Checkbox, Requests

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Tasks.css';
-import { GetTasksForUser, CreateTask, DeleteTask } from '../Requests/TaskRequest';
+import { GetTasksForUser, CreateTask, DeleteTask, UpdateTask } from '../Requests/TaskRequest';
 import Button from '@mui/material/Button';
 import { Checkbox } from '@mui/material/';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
@@ -10,7 +10,6 @@ function TasksPage() {
 
 	const userid = 1;
 	const [todos, setTodos] = React.useState([]);
-	const [checked, setChecked] = React.useState(false);
 	const [value, setValue] = React.useState([]);
 	// const [input, setInput] = React.useState([]);
 
@@ -25,15 +24,41 @@ function TasksPage() {
 	}
 
 	const addTasks = async () => {
-		const body = await CreateTask("uwu", value, userid);
+		const body = await CreateTask(value, false, userid);
 		setTodos(todos.concat(body.data));
 	}
 
-	const deleteTask = async(todoIndex) => {
-		await DeleteTask(todoIndex);
-		setTodos(todos.filter(x => x.id !== todoIndex));
+	const deleteTask = async(id) => {
+		await DeleteTask(id);
+		setTodos(todos.filter(x => x.id !== id));
 	}
 
+	// const completeTask = todos.findIndex( (todoIndex) => { if (todo.id === id) 
+  //     return todo;}
+	// );
+  // const completeTodo = id => {
+  //   let updatedTodos = todos.map(todo => {
+  //     if (todo.id === id) {
+  //       todo.isComplete = !todo.isComplete;
+  //     }
+  //     return todo;
+  //   });
+  //   setTodos(updatedTodos);
+  // };
+	const setTaskChecked = (task) => {
+		setTodos(prev => prev.map(x => {
+			if(x.id === task.id)
+			{
+				x.done = x.done === 1 ? 0 : 1;
+			}
+			return x;
+		}));
+		updateTask(task);
+	}
+
+ 	const updateTask = async(task) => {
+		 await UpdateTask(task.id,task.content,task.done,task.userid);
+	}
 
 	return (
 		<div className='tasks'>
@@ -57,7 +82,7 @@ function TasksPage() {
 									<li className='task-li'>		
 										<Grid container justifyContent="space-between">
 											<Grid item >
-												<Checkbox key={todo.id} checked={checked} onChange={(event) => setChecked(event.target.checked)}/>
+												<Checkbox key={todo.id} checked={todo.done === 1 ? true : false} onChange={() => {setTaskChecked(todo)}}/>
 													{todo.content}{todo.id}
 											</Grid>
 											<Grid item xs={2}>
