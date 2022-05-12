d9b3c4f3e18ba889ae6efcb8e13dc7a90b559bb2
mirozy13 <89573948+mirozy13@users.noreply.github.com>
2022-05-12 16:43:42 +0700

    feat: added mui

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -6,8 +6,8 @@ function RegistrationPage() {
 	const [password, setPassword] = useState('')
 	const [emailDirty, setEmailDirty] = useState(false)
 	const [passwordDirty, setPasswordDirty] = useState(false)
-	const [emailError, setEmailError] = useState('e-mail не может быть пустым')
-	const [passwordError, setPasswordError] = useState('Пароль не может быть пустым')
+	const [emailError, setEmailError] = useState("e-mail can't be empty")
+	const [passwordError, setPasswordError] = useState("password can't be empty")
 
 	const blurHandler = (e) => {
 		switch (e.target.name) {
@@ -20,45 +20,56 @@ function RegistrationPage() {
 			default:
 				break
 		}
-	}
+	};
+
 	return (
 		<form>
-			<Stack direction="column" className="App" style={{width: "50%", margin: "auto"}}>
+			<Stack direction="column" className="App" style={{width: "60%", margin: "auto"}}  spacing={2}>
 			<h1>Sign up</h1>
-			<TextField
-				name='username'
-				type='text'
-				placeholder='Enter your username...'
-				variant="standard"
-				className="StandardInput"
+				<TextField
+					name='username'
+					type='text'
+					placeholder='Enter your username...'
+					variant="standard"
+					className="StandardInput"
 			/>
-				{/*{(emailDirty && emailError) &&*/}
-				{/*	<div style={{color:'red'}}>*/}
-				{/*		(emailError)*/}
-				{/*	</div>*/}
-				{/*}*/}
-			<TextField
-				onBlur={e=>blurHandler(e)}
-				name ='email'
-				type='text'
-				placeholder='Enter your email...'/>
+				{(emailDirty && emailError) &&
+					<div style={{color:'red'}}>
+						(emailError)
+					</div>
+				}
+
+				<TextField
+					onBlur={e=>blurHandler(e)}
+					name ='email'
+					type='text'
+					className="StandardInput"
+					variant='standard'
+					placeholder='Enter your email...'
+				/>
+				
 				{(passwordDirty && passwordError) &&
 				<div style={{color:'red'}}>
 					(passwordError)
 				</div>
 				}
-			<TextField
-				onBlur={e=>blurHandler(e)}
-				name='password'
-				type='password'
-				placeholder='Enter your password...'/>
-			<TextField
-				name='password2'
-				type='password'
-				placeholder='Repeat your password...'/>
-			<Button type='submit'>
+
+				<TextField
+					onBlur={e=>blurHandler(e)}
+					name='password'
+					type='password'
+					className="StandardInput"
+					variant='standard'
+					placeholder='Enter your password...'/>
+				<TextField
+					name='password2'
+					type='password'
+					className="StandardInput"
+					variant='standard'
+					placeholder='Repeat your password...'/>
+				<Button type='text'>
 				Sign up
-			</Button>
+				</Button>
 			</Stack>
 		</form>
 	);
